08b79f5f2a180b54ce7d88cfdd8811fb591b39c7
João VIctor <joawrt8520@gmail.com>
2024-07-29 14:25:33 -0300

    feat: BookDaoJDBC: fundByStudantUser implementation

---
+++ src/model/dao/impl/BookDaoJDBC.java
@@ -0,0 +1,62 @@
+package model.dao.impl;
+
+import database.DBException;
+import model.dao.BookDao;
+import model.entities.Book;
+import model.entities.StudentUser;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BookDaoJDBC implements BookDao {
+
+    private Connection conn;
+
+    public BookDaoJDBC (Connection conn){
+        this.conn = conn;
+    }
+
+    @Override
+    public List<Book> findByStudentId(StudentUser user){
+
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        try{
+
+            ps = conn.prepareStatement(
+                    "select * from book "
+                       +"where StudentId = ?"
+            );
+
+            ps.setInt(1, user.getId());
+            rs = ps.executeQuery();
+
+            List<Book> list = new ArrayList<>();
+
+            while (rs.next()){
+                list.add(instantiateBook(rs));
+            }
+
+            return list;
+        }
+        catch (SQLException e){
+            throw new DBException(e.getMessage());
+        }
+    }
+
+    // instantiate Book
+    private static Book instantiateBook(ResultSet rs) throws SQLException {
+        Book book = new Book();
+        book.setId(rs.getInt("Id"));
+        book.setTitle(rs.getString("Title"));
+        book.setWithdrawDate(LocalDateTime.parse(rs.getDate("WithDrawDate").toString()));
+        book.setDeliveryDate(LocalDateTime.parse(rs.getDate("DeliveryDate").toString()));
+        return book;
+    }
+}
